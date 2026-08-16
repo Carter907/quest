a4dae3674a948fe0b94bdab04d9e0e2aa61c35e4
Carter <speers.carter@gmail.com>
2026-08-15 20:04:52 -0400

    feat(cmd): added 'new' subcommand
    - create a new knowledge graph using the new subcommand
    
    Signed-off-by: Carter <speers.carter@gmail.com>

--- cmd/new.go
+++ cmd/new.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -10,32 +12,58 @@ import (
 var newCmd = &cobra.Command{
 	Use:   "new",
 	Short: "create a new knowledge graph directory",
-	Long: `this command is how you start a new knowledge graph from the command line. You specifiy the name
-	of the knowledge graph and a directory will be created with a single guide using default values for
-	the frontmatter. These default values can be configured. If you don't specify a directory, the current directory
-	will be used.
+	Long: `You start a new knowledge graph with the new command. You only have to specify the name of the knowledge graph;
+	a directory will be created. A starter guide will be added in the directory as a template.
+
+	If you don't specify a directory, the current directory will be used.
+
 	Examples:
+
 		qst new learn-cpp
 
 	if you don't specify a directory, the current one will be used:
-		qst new
 
+		qst new
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("new called")
+		dir := "."
+		if len(args) != 0 {
+			dir = args[0]
+		}
+
+		err := os.MkdirAll(dir, 0o755)
+		if err != nil {
+			fmt.Printf("Failed to create knowledge graph: %v\n", err)
+			os.Exit(1)
+		}
+
+		starterPath := filepath.Join(dir, "starter.md")
+		if _, err := os.Stat(starterPath); os.IsNotExist(err) {
+			starterContent := `---
+prerequisites: []
+sub_guides: []
+clarity: strict
+scope: definition
+tags: ["example"]
+---
+
+## Starter Guide
+
+This is an example guide. Replace this content with your own knowledge!
+`
+			err = os.WriteFile(starterPath, []byte(starterContent), 0o644)
+			if err != nil {
+				fmt.Printf("Failed to create starter guide: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("Initialized empty knowledge graph in %s\n", dir)
+			fmt.Printf("Created starter guide at %s\n", starterPath)
+		} else {
+			fmt.Printf("Knowledge graph initialized in %s\n", dir)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(newCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// newCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// newCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
